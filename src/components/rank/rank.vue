648dfda9fc632fe73171b043f51a52235469e00b
Your Name <you@example.com>
2019-01-15 00:41:54 +0800

    头部tab导航添加

---
+++ src/components/rank/rank.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+      这是rank页面
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
+import router from './router'
 import fastclick from 'fastclick'
 
 // Vue.config.productionTip = false
@@ -13,5 +14,7 @@ fastclick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  // router挂载到Vue实例上
+  router,
   render: h => h(App)
 })
